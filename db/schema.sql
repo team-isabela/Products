ee4b34e2760afd0ce83e25898885ad3d8b358d23
Salar Malik <salarxmalik@gmail.com>
2022-01-15 15:28:53 -0800

    Updated Queries to Work

--- db/schema.sql
+++ db/schema.sql
@@ -1,42 +1,56 @@
+\c postgres;
 
-DROP DATABASE IF EXISTS productsdb;
+DROP DATABASE IF EXISTS products;
 
-CREATE DATABASE productsdb;
+CREATE DATABASE products;
+
+\c products;
 
 CREATE TABLE product (
-  id INT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  slogan VARCHAR NULL DEFAULT NULL,
-  description VARCHAR NULL DEFAULT NULL,
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(500) NOT NULL,
+  slogan TEXT NULL,
+  description TEXT NULL,
   category VARCHAR(255) NOT NULL,
-  default_price INT NOT NULL
+  default_price VARCHAR(255) NOT NULL
 );
+
+\copy product FROM '/home/salarmalik/SDC_Files/product.csv' WITH (FORMAT csv, HEADER TRUE);
 
 CREATE TABLE related (
-  id INT NOT NULL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
   current_product_id INT NOT NULL,
   related_product_id INT NOT NULL,
-  FOREIGN KEY (current_product_id) REFERENCES product (id),
-  FOREIGN KEY (related_product_id) REFERENCES product (id)
+  FOREIGN KEY (current_product_id) REFERENCES product (id)
 );
 
+\copy related(id, current_product_id, related_product_id) FROM '/home/salarmalik/SDC_Files/related.csv' WITH (FORMAT csv, HEADER TRUE);
+
 CREATE TABLE features (
-  id INT NOT NULL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   product_id INT NOT NULL,
   feature VARCHAR(100) NOT NULL,
   value VARCHAR(255) NULL,
   FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
+\copy features(id, product_id, feature, value) FROM '/home/salarmalik/SDC_Files/features.csv' WITH (FORMAT csv, HEADER TRUE);
+
 CREATE TABLE styles (
   id SERIAL PRIMARY KEY,
   productId INT NOT NULL,
   name VARCHAR(500) NOT NULL,
-  sale_price INT NULL DEFAULT 0,
-  original_price INT NULL DEFAULT 0,
-  default_style SMALLINT DEFAULT 0,
+  sale_price VARCHAR(255) NULL,
+  original_price VARCHAR(255) NOT NULL,
+  default_style SMALLINT,
   FOREIGN KEY (productId) REFERENCES product (id)
 );
+
+\copy styles(id, productId, name, sale_price, original_price, default_style) FROM '/home/salarmalik/SDC_Files/styles.csv' WITH (FORMAT csv, HEADER TRUE);
+
+ALTER TABLE styles ALTER COLUMN default_style DROP DEFAULT;
+ALTER TABLE styles ALTER default_style TYPE bool USING CASE WHEN default_style=0 THEN FALSE ELSE TRUE END;
+ALTER TABLE styles ALTER COLUMN default_style SET DEFAULT FALSE;
 
 CREATE TABLE photos (
   id SERIAL PRIMARY KEY,
@@ -46,6 +60,8 @@ CREATE TABLE photos (
   FOREIGN KEY (styleId) REFERENCES styles (id)
 );
 
+\copy photos(id, styleId, url, thumbnail_url) FROM '/home/salarmalik/SDC_Files/photos.csv' WITH (FORMAT csv, HEADER TRUE);
+
 CREATE TABLE skus (
   id SERIAL PRIMARY KEY,
   styleId INT NOT NULL,
@@ -53,3 +69,13 @@ CREATE TABLE skus (
   quantity INT NOT NULL DEFAULT 0,
   FOREIGN KEY (styleId) REFERENCES styles (id)
 );
+
+\copy skus(id, styleId, size, quantity) FROM '/home/salarmalik/SDC_Files/skus.csv' WITH (FORMAT csv, HEADER TRUE);
+
+CREATE INDEX product_id_index ON product (id);
+CREATE INDEX related_productId_index ON related (current_product_id);
+CREATE INDEX features_productId_index ON features (product_id);
+CREATE INDEX styles_id_index ON styles (id);
+CREATE INDEX styles_productId_index ON styles (productId);
+CREATE INDEX photos_styleId_index ON photos (styleId);
+CREATE INDEX skus_styleId_index ON skus (styleId);
